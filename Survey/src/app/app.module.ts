1c0ad7e25432f75215570f5a15d654139e733cac
ZawMinbliss <zawmin.thein.u20@bliss-stock.com>
2022-05-09 20:37:47 +0630

    added form

--- Survey/src/app/app.module.ts
+++ Survey/src/app/app.module.ts
@@ -4,17 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SurveyFormComponent } from './survey-form/survey-form.component';
-
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 @NgModule({
-  declarations: [
-    AppComponent,
-    SurveyFormComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, SurveyFormComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- Survey/src/app/survey-form/survey-form.component.ts
+++ Survey/src/app/survey-form/survey-form.component.ts
@@ -1,12 +1,30 @@
 import { Component, OnInit } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'survey-form',
   templateUrl: './survey-form.component.html',
   styleUrls: ['./survey-form.component.css'],
 })
 export class SurveyFormComponent implements OnInit {
-  constructor() {}
-
+  constructor(private http: HttpClient) {}
+  token: String | undefined;
+  valid: String | undefined;
   ngOnInit(): void {}
+  onSubmit(data: any): void {
+    console.log(data);
+    if (data.status == 'INVALID') {
+      this.token = undefined;
+      this.valid = 'INVALID';
+    } else {
+      this.http
+        .post('http://localhost:3300/api/survey', data.value)
+        .subscribe((results: any) => {
+          console.warn('results:', results);
+          this.token = results.Token;
+          console.log(this.token);
+        });
+
+      console.log('from submit');
+    }
+  }
 }
