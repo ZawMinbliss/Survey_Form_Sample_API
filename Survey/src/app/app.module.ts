b289bd00a5372338a8449f9674a8d8ef29ae6daf
ZawMinbliss <zawmin.thein.u20@bliss-stock.com>
2022-05-09 17:09:51 +0630

    create survey form component

--- Survey/src/app/app.module.ts
+++ Survey/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SurveyFormComponent } from './survey-form/survey-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SurveyFormComponent
   ],
   imports: [
     BrowserModule,
